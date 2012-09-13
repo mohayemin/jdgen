3d632319bd6d1001394bcfcc4c0e5a1733eab27f
mohayemin <bit0102@iit.du.ac.bd>
2012-09-13 22:31:23 +0600

    1. Collapse hierarchy: AbstractConstrainableBuilder merged to ObjectBuilder
    2. ListBuilder is removed from abstraction. Will be added to
    implementation.

--- jDGenCore/src/edu/iitdu/jdgen/builder/implementation/ObjectBuilder.java
+++ jDGenCore/src/edu/iitdu/jdgen/builder/implementation/ObjectBuilder.java
@@ -1,17 +1,89 @@
 package edu.iitdu.jdgen.builder.implementation;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
 
+import edu.iitdu.jdgen.builder.abstraction.ConstrainableBuilder;
 import edu.iitdu.jdgen.exception.JDGenRuntimeException;
+import edu.iitdu.jdgen.reflection.ConstructorInvoker;
 import edu.iitdu.jdgen.reflection.MethodInvoker;
+import edu.iitdu.jdgen.util.MethodUtils;
 
 /**
  * @author Mohayeminul Islam
  */
-public class ObjectBuilder<T> extends AbstractConstrainableBuilder<T> {
+public class ObjectBuilder<T> implements ConstrainableBuilder<T> {
+
+	private Class<T> type;
+	protected ConstructorInvoker<T> constructor;
+	protected List<MethodInvoker<T>> methods;
+	protected List<MethodInvoker<T>> setters;
 
 	public ObjectBuilder(Class<T> type) {
-		super(type);
+		this.type = type;
+		try {
+			constructor = new ConstructorInvoker<T>(type.getConstructor());
+		} catch (Exception e) {
+			constructor = null;
+		}
+
+		methods = new LinkedList<>();
+		setters = new LinkedList<>();
+	}
+
+	@Override
+	public ConstrainableBuilder<T> execute(MethodInvoker<T> method) {
+		methods.add(method);
+
+		return this;
+	}
+
+	@Override
+	public ConstrainableBuilder<T> execute(String methodName,
+		Object... arguments) throws JDGenRuntimeException {
+		try {
+			MethodInvoker<T> invoker =
+				new MethodInvoker<T>(type, methodName, arguments);
+			return execute(invoker);
+		} catch (NoSuchMethodException e) {
+			throw new JDGenRuntimeException(e);
+		} catch (SecurityException e) {
+			throw new JDGenRuntimeException(e);
+		}
+	}
+
+	@Override
+	public ConstrainableBuilder<T> construct(
+		ConstructorInvoker<T> constructor) {
+		this.constructor = constructor;
+		return this;
+	}
+
+	@Override
+	public ConstrainableBuilder<T> construct(Object... arguments)
+		throws JDGenRuntimeException {
+		try {
+			ConstructorInvoker<T> constructor =
+				new ConstructorInvoker<T>(type, arguments);
+			return construct(constructor);
+		} catch (NoSuchMethodException e) {
+			throw new JDGenRuntimeException(e);
+		}
+	}
+
+	@Override
+	public <U> ConstrainableBuilder<T> set(String setterVariableName, U value) {
+		try {
+			Method setter = MethodUtils.findSetter(type, setterVariableName);
+			MethodInvoker<T> setterInvoker = new MethodInvoker<>(setter, value);
+			setters.add(setterInvoker);
+			return this;
+		} catch (NoSuchMethodException e) {
+			throw new JDGenRuntimeException(e);
+		}
+
 	}
 
 	/**
@@ -27,7 +99,6 @@ public class ObjectBuilder<T> extends AbstractConstrainableBuilder<T> {
 		return object;
 	}
 
-	@Override
 	protected void callMethods(T object) throws JDGenRuntimeException {
 		try {
 			for (MethodInvoker<T> method : methods) {
@@ -39,19 +110,21 @@ public class ObjectBuilder<T> extends AbstractConstrainableBuilder<T> {
 		}
 	}
 
-	@Override
 	protected T construct() throws JDGenRuntimeException {
 		try {
+			if (constructor == null) {
+				constructor = new ConstructorInvoker<T>(type.getConstructor());
+			}
 			T object = constructor.invoke();
 
 			return object;
 		} catch (InstantiationException | IllegalAccessException
-			| IllegalArgumentException | InvocationTargetException e) {
+			| IllegalArgumentException | InvocationTargetException
+			| NoSuchMethodException e) {
 			throw new JDGenRuntimeException(e);
 		}
 	}
 
-	@Override
 	protected void callSetters(T object) throws JDGenRuntimeException {
 		try {
 			for (MethodInvoker<T> setter : setters) {

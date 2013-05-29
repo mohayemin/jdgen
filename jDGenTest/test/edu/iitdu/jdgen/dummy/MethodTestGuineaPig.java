62e3243c4ade11bf7ddf609cbfbf2d9d34af2530
mohayemin <mohayeminul.islam@gmail.com>
2013-05-30 03:25:25 +0600

    A Guinea Pig class for method test is implemented.

---
+++ jDGenTest/test/edu/iitdu/jdgen/dummy/MethodTestGuineaPig.java
@@ -0,0 +1,33 @@
+package edu.iitdu.jdgen.dummy;
+
+public interface MethodTestGuineaPig {
+	/**
+	 * Not setter
+	 */
+	public void set(int val);
+
+	/**
+	 * Not setter
+	 */
+	public void notASetter(int val);
+
+	/**
+	 * Not setter
+	 */
+	public void setVal();
+
+	/**
+	 * Not setter
+	 */
+	public void setval(int val);
+
+	/**
+	 * Setter
+	 */
+	public void setVal(int val);
+
+	/**
+	 * Setter
+	 */
+	public long setVal(long val);
+}
